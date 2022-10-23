8133b4ddcaecf843e7769b2c3d4751634e44e0e8
SoundCampsMichaelv <86958340+Michael-Verthin@users.noreply.github.com>
2022-10-23 16:25:40 +0300

    I've added a simple Hello World file

---
+++ src/App.java
@@ -0,0 +1,9 @@
+/*
+ * App.java
+ * This is the basic app that came with JAVA for VSCode
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
