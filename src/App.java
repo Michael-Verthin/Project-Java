d58c02be8acee60cb50b696be9fac0ed555b857d
SoundCampsMichaelv <86958340+Michael-Verthin@users.noreply.github.com>
2022-10-23 19:13:54 +0300

    num1 and num2 added

--- src/App.java
+++ src/App.java
@@ -5,5 +5,8 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int num1 = 1;
+        int num2 = num1 + 1;
+        System.out.println("num2 is = " + num2);
     }
 }
