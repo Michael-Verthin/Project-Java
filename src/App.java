d1cc6cfa46a732517f3c07d3bd0b177309171664
SoundCampsMichaelv <86958340+Michael-Verthin@users.noreply.github.com>
2022-12-20 15:35:12 +0200

    20.12.22

--- src/App.java
+++ src/App.java
@@ -1,12 +1,55 @@
+
 /*
  * App.java
  * This is the basic app that came with JAVA for VSCode
  */
+import java.util.Scanner; // Import the Scanner class
+
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        int num1 = 1;
-        int num2 = num1 + 1;
-        System.out.println("num2 is = " + num2);
+        // int[] arr1 = new int[5];
+        // double[] arr = { 1.111, 100.52, 10.7, 13.9, 0.99, 0.42, 1.3 };
+        // for (int i = 0; i < arr.length; i++) {
+        // for (int z = i + 1; z < arr.length; z++) {
+        // if (arr[i] + arr[z] == (int) (arr[i] + arr[z]))
+        // System.out.println(true);
+        // }
+        // }
+        System.out.println((123456786 / 1000000) % 10);
     }
+
+    // public static void main(String[] args) {
+    // System.out.println("Hello, World!\n");
+    // int age;
+    // double salary;
+    // Scanner scan = new Scanner(System.in);
+    // System.out.println("Please enter your age");
+    // age = scan.nextInt();
+    // System.out.println();
+    // salary = scan.nextDouble();
+    // if (age > 18 && age < 67)
+    // salary = salary - 100;
+    // System.out.println("Your salary is: " + salary);
+    // }
+    /*
+     * Scanner scan = new Scanner(System.in); // Create a Scanner object
+     * double height;
+     * double width;
+     * System.out.println(987.0 / 100);
+     * System.out.
+     * println("This program is calculating the parameter and the area of the given height and width"
+     * );
+     * System.out.print("Please enter the Rectangle height: ");
+     * height = scan.nextDouble();
+     * System.out.print("Please enter the Rectangle width: ");
+     * width = scan.nextDouble();
+     * System.out.println("The Parameter of the Rectangle is: " + (2 * height + 2 *
+     * width));
+     * System.out.println("The Area of the Rectable is: " + width * height);
+     * System.out.println(num % 10); // אחדות
+     * System.out.println(num / 10 % 10); // עשרות
+     * System.out.println(num / 100); // מאות
+     */
+
 }
